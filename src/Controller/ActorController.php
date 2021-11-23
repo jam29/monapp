212af9d7a3694ab93ff979318dd3cab8fc56a0c2
Jam <jam29n@gmail.com>
2021-11-23 16:14:16 +0100

    Modification et creation d'un acteur

--- src/Controller/ActorController.php
+++ src/Controller/ActorController.php
@@ -5,8 +5,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-use App\Repository\ActorRepository;
-use App\Entity\Actor;
+use App\Repository\ActorRepository ;
+use App\Entity\Actor ;
+use App\Form\ActorType ;
+
+use Symfony\Component\HttpFoundation\Request; 
+use Doctrine\ORM\EntityManagerInterface;
 
 class ActorController extends AbstractController
 {
@@ -30,6 +34,31 @@ class ActorController extends AbstractController
         
       //  $actor = $ar->find($id); 
         return $this->render('actor/afficherUn.html.twig',["actor"=>$actor]);
+    }
+
+    /**
+     * @Route("/modifactor/{id}", name="modifactor")
+     * @Route("/creationactor", name="creationactor")
+     */
+    public function modifierActor(Actor $actor=null, Request $request, EntityManagerInterface $em ) {
+
+        if (!$actor ) {
+            $actor = new Actor();
+        }
+        
+        $form = $this->createForm(ActorType::class,$actor);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() ) {
+            $em->persist($actor) ;
+            $em->flush(); 
+            return $this->redirectToRoute('actor') ;
+        }
+       
+        return $this->render('actor/modifActor.html.twig',[
+            "actor"=>$actor,
+            "form"=>$form->createView()
+        ]);
     }
 
 
